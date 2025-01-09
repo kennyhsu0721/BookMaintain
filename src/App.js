8c35a234cc1008f1707f9e04b17b9dc2f082c4b6
GSS_DOMAIN\kenny_hsu <kenny_hsu@gss.com.tw>
2025-01-09 11:02:44 +0800

    New Create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
       </header>
     </div>
@@ -23,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+// import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BookMaintain from './BookMaintain'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const vm={
+  "BookName":"",
+  "BookAuthor":""
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <><header>
+    <nav className="navbar">
+      <h1 className="navbar-title text-center">圖書管理系統</h1>
+    </nav>
+  </header>
+    <main>
+      <BookMaintain></BookMaintain>
+    </main></>
 );
 
 // If you want to start measuring performance in your app, pass a function
